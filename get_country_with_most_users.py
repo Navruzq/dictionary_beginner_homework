62d0faf61e519e65ab1cabd05274d3a8b87e7fb0
Navruzq <qarshiyevnavruz7777@gmail.com>
2022-11-22 21:55:48 +0500

    get_country_with_most_users

--- get_country_with_most_users.py
+++ get_country_with_most_users.py
@@ -7,4 +7,11 @@ def get_country_with_most_users(data:list) -> str:
     Returns:
         str: The country with the most users
     """
-    pass
+    a=data[0]['country']
+    b=data[1]['country']
+    if len(a)<len(b):
+        return b
+    else:
+        return a
+    
+print(get_country_with_most_users([{'name': 'John', 'country': 'USA'}, {'name': 'Mary', 'country': 'UK'}]))
